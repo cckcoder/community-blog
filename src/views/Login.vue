111cb606d764e8c257fa797299935eb6a810211d
codewiz <c.chaiwat.ch@gmail.com>
2019-08-04 23:18:04 +0700

    Using router link.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>LOGIN COMPONENT</h1>
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<div class="card">
+				<div class="card-body">
+					<h3 class="text-center my-4">Login</h3>
+					<div class="form-group">
+						<input class="form-control" placeholder="Email" type="TEXT">
+					</div>
+					<div class="form-group">
+						<input class="form-control" placeholder="Password" type="PASSWORD">
+					</div>
+					<div class="form-group text-center">
+						<button class="btn btn-success">Login</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
